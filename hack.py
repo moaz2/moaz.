690fb253812d0f8f5bf110f0e3a3f5272d4ffc2a
moaz2 <51852939+moaz2@users.noreply.github.com>
2019-06-15 11:45:15 +0000

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,11 +1,11 @@
 import socket
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("127.0.0.1",5000))
+    s.connect(("10.0.0.141",5000))
     while True:
         data = str(input("data > "))
         s.send(data.encode("utf-8"))
-        s.sendto(data.encode("utf-8"),("127.0.0.1",5000))
+        s.sendto(data.encode("utf-8"),("10.0.0.141",5000))
         print(s.recvfrom(1024))
     s.close
 except socket.error as e:
